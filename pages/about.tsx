faeaa6f67eb7913c66543f46dd7ee0648c62cbec
freeshineit <xiaoshaoqq@gmail.com>
2020-11-29 14:38:55 +0800

    fix: unless handle

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,12 +1,10 @@
 import Link from "next/link";
-import { useState, useEffect, useCallback } from "react";
+import { useState, useEffect } from "react";
 import Layout from "../components/Layout";
 import Com from "../components/Com";
 
 const AboutPage = () => {
   const [timer, setTimer] = useState(0);
-
-  const handle = useCallback(() => {}, []);
 
   useEffect(() => {
     const t = setInterval(() => {
